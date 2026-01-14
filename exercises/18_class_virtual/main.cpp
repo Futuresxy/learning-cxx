7bafd2b654c824ee3c536fa75808d4eb7bf6f53d
songxiaoyou <songxiaoyou@whu.edu.cn>
2026-01-15 00:49:56 +0800

    c++学习

--- exercises/18_class_virtual/main.cpp
+++ exercises/18_class_virtual/main.cpp
@@ -12,7 +12,7 @@ struct A {
 };
 struct B : public A {
     // READ: override <https://zh.cppreference.com/w/cpp/language/override>
-    char virtual_name() const override {
+    char virtual_name() const override { //为了保证重写正确，一般要求在后面增加 override 或 final 修饰符。override 表示一般重写，
         return 'B';
     }
     char direct_name() const {
@@ -21,7 +21,7 @@ struct B : public A {
 };
 struct C : public B {
     // READ: final <https://zh.cppreference.com/w/cpp/language/final>
-    char virtual_name() const final {
+    char virtual_name() const final {  //final 表示终结重写，即不可以在被以后的孙子类重写
         return 'C';
     }
     char direct_name() const {
@@ -41,39 +41,48 @@ int main(int argc, char **argv) {
     B b;
     C c;
     D d;
+    // std::cout<<a.virtual_name()<<"a.virtual_name()"<< std::endl ;
+    // std::cout<<b.virtual_name()<<"b.virtual_name()"<< std::endl ;
+    // std::cout<<c.virtual_name()<<"c.virtual_name()"<< std::endl ;
+    // std::cout<<d.virtual_name()<<"d.virtual_name()"<< std::endl ;
+    // std::cout<<a.direct_name()<<"a.direct_name()"<< std::endl ;
+    // std::cout<<b.direct_name()<<"b.direct_name()"<< std::endl ;
+    // std::cout<<c.direct_name()<<"c.direct_name()"<< std::endl ;
+    // std::cout<<d.direct_name()<<"d.direct_name()"<< std::endl ;
 
-    ASSERT(a.virtual_name() == '?', MSG);
-    ASSERT(b.virtual_name() == '?', MSG);
-    ASSERT(c.virtual_name() == '?', MSG);
-    ASSERT(d.virtual_name() == '?', MSG);
-    ASSERT(a.direct_name() == '?', MSG);
-    ASSERT(b.direct_name() == '?', MSG);
-    ASSERT(c.direct_name() == '?', MSG);
-    ASSERT(d.direct_name() == '?', MSG);
+
+    ASSERT(a.virtual_name() == 'A', MSG);
+    ASSERT(b.virtual_name() == 'B', MSG);
+    ASSERT(c.virtual_name() == 'C', MSG);
+    ASSERT(d.virtual_name() == 'C', MSG);
+    ASSERT(a.direct_name() == 'A', MSG);
+    ASSERT(b.direct_name() == 'B', MSG);
+    ASSERT(c.direct_name() == 'C', MSG);
+    ASSERT(d.direct_name() == 'D', MSG);
 
     A &rab = b;
     B &rbc = c;
     C &rcd = d;
 
-    ASSERT(rab.virtual_name() == '?', MSG);
-    ASSERT(rbc.virtual_name() == '?', MSG);
-    ASSERT(rcd.virtual_name() == '?', MSG);
-    ASSERT(rab.direct_name() == '?', MSG);
-    ASSERT(rbc.direct_name() == '?', MSG);
-    ASSERT(rcd.direct_name() == '?', MSG);
+    ASSERT(rab.virtual_name() == 'B', MSG);
+    ASSERT(rbc.virtual_name() == 'C', MSG);
+    ASSERT(rcd.virtual_name() == 'C', MSG);
+    ASSERT(rab.direct_name() == 'A', MSG);
+    ASSERT(rbc.direct_name() == 'B', MSG);
+    ASSERT(rcd.direct_name() == 'C', MSG);
 
     A &rac = c;
     B &rbd = d;
 
-    ASSERT(rac.virtual_name() == '?', MSG);
-    ASSERT(rbd.virtual_name() == '?', MSG);
-    ASSERT(rac.direct_name() == '?', MSG);
-    ASSERT(rbd.direct_name() == '?', MSG);
+    ASSERT(rac.virtual_name() == 'C', MSG);
+    ASSERT(rbd.virtual_name() == 'C', MSG);
+    ASSERT(rac.direct_name() == 'A', MSG);
+    ASSERT(rbd.direct_name() == 'B', MSG);
 
     A &rad = d;
 
-    ASSERT(rad.virtual_name() == '?', MSG);
-    ASSERT(rad.direct_name() == '?', MSG);
+    ASSERT(rad.virtual_name() == 'C', MSG);
+    ASSERT(rad.direct_name() == 'A', MSG);
 
     return 0;
 }
--- exercises/20_function_template/main.cpp
+++ exercises/20_function_template/main.cpp
@@ -2,7 +2,8 @@
 
 // READ: 函数模板 <https://zh.cppreference.com/w/cpp/language/function_template>
 // TODO: 将这个函数模板化
-int plus(int a, int b) {
+template<class T>
+T plus(T a, T b) {
     return a + b;
 }
 
@@ -14,7 +15,11 @@ int main(int argc, char **argv) {
     ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
     ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
     // TODO: 修改判断条件使测试通过
-    ASSERT(plus(0.1, 0.2) == 0.3, "How to make this pass?");
+    std::cout<< (plus(0.1, 0.2) == 0.3) <<std::endl;  
+    //0.1 + 0.2 ≠ 精确的 0.3  因为浮点数无法精确表示十进制的小数,
+    //在double fp64下存在多位尾数，相加后精度能够体现差异
+    //加上f后是fp32通过舍人等操作发现刚好相等
+    ASSERT(std::abs(plus(0.1f, 0.2f) - 0.3f )<1e-9 , "How to make this pass?");
 
     return 0;
 }
